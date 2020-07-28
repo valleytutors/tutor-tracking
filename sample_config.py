1953c2afe33e0ca02216f898af14b1ebaaa3ebeb
Pacman99 <pachum99@gmail.com>
2020-07-28 10:59:55 -0700

    init sample config file

---
+++ sample_config.py
@@ -0,0 +1,25 @@
+# Set these to the tutor and tutee satisfaction form spreadsheet url
+TUTOR_SPREADSHEET_URL = ""
+TUTEE_SPREADSHEET_URL = "";
+
+# Headers for the spreadsheets
+TUTOR_HEADERS = {
+	"name": "Name",
+	"tutee": "Who did you tutor?",
+	"minutes": "How long was the tutoring session",
+	"rating": "How would you rate your tutee(s)?",
+	"completion": "Check this box to verify you had your meeting",
+	"timestamp": "Timestamp",
+}
+
+TUTEE_HEADERS = { 
+	"name": "Student's Name",
+	"tutor": "Tutor(s)' Name",
+	"minutes": "How long was your session?",
+	"rating": "How would you rate your tutor(s)",
+	"completion": "Check the box to verify the session was completed",
+	"timestamp": "Timestamp",
+	"wentWell": "What went well?",
+	"wentPoorly": "What went poorly?"
+}
+
