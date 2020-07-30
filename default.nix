a6f38ad706a3936593042745c622fb1b11277890
Pacman99 <pachum99@gmail.com>
2020-07-29 23:33:52 -0700

    updated dev environment to use mach-nix added rope(refactoring) to dev environment

--- default.nix
+++ default.nix
@@ -1,15 +1,13 @@
-{ pkgs ? import <nixpkgs> {} }:
 let
-#  python = import ./requirements.nix { inherit pkgs; };
-  gspread_fixed = pkgs.python38Packages.gspread.overrideAttrs (old: {
-    buildInputs = [ pkgs.python38Packages.google-auth-oauthlib ];
+  mach-nix = import (builtins.fetchGit {
+    url = "https://github.com/DavHau/mach-nix/";
+    ref = "2.0.0";
   });
-in 
-pkgs.mkShell {
-  buildInputs = with pkgs.python38Packages; [ 
+in
+mach-nix.mkPythonShell {
+  requirements = ''
+    notion
+    gspread
     google-auth-oauthlib
-    oauth2client
-    gspread_fixed
-  ];
+  '';
 }
-
