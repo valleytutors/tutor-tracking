9896572d8fbd961541499a9d725fc015104588c6
Pacman99 <pachum99@gmail.com>
2020-12-01 19:15:25 -0800

    updated sample config

--- sample_config.py
+++ sample_config.py
@@ -1,25 +1,45 @@
-# Set these to the tutor and tutee satisfaction form spreadsheet url
-TUTOR_SPREADSHEET_URL = ""
-TUTEE_SPREADSHEET_URL = "";
+NEEDED_MATCHING_RATIO = .85
+NEEDED_TIMESTAMP_DIFF = 7
 
-# Headers for the spreadsheets
+
+NOTION_AUTH_TOKEN = "alsdfjalskdfjsadlfjasdkl;"
+
+# Used for email sending
+SENDER_EMAIL = "example@example.org"
+SENDER_PASSWORD = "asldkfjasdl;fjsd;l"
+
+ORGANIZATION_NAME = ""
+
+NOTION_TUTOR_PAGE = "https://www.notion.so/sldjfds"
+NOTION_TUTEE_PAGE = "hhttps://www.notion.so/laskdfjlasdkfj"
+# Data page is most likely not used anymore, will be removed eventually
+NOTION_DATA_PAGE = "https://www.notion.so/ldfjsdlkfj"
+
+DATA_SPREADSHEET_URL = "https://docs.google.com/spreadsheets/d/slabasdf"
+DATA_FOLDER_ID = "shdgiowerqoa"
+
+TUTOR_SPREADSHEET_URL = "https://docs.google.com/spreadsheets/d/wekrljntermtni"
+TUTEE_SPREADSHEET_URL = "https://docs.google.com/spreadsheets/d/kadfjlksfjnwertn"
+
 TUTOR_HEADERS = {
-	"name": "Name",
-	"tutee": "Who did you tutor?",
-	"minutes": "How long was the tutoring session",
-	"rating": "How would you rate your tutee(s)?",
-	"completion": "Check this box to verify you had your meeting",
-	"timestamp": "Timestamp",
+    "name": "Name",
+    "tutee": "Who did you tutor?",
+    "length": "How long was the tutoring session (in MINUTES)",
+    "rating": "How would you rate your tutee(s)?",
+    "completion": "Check this box to verify you had your meeting",
+    "timestamp": "Timestamp",
+    "date": "When was your session?"
 }
 
 TUTEE_HEADERS = { 
-	"name": "Student's Name",
-	"tutor": "Tutor(s)' Name",
-	"minutes": "How long was your session?",
-	"rating": "How would you rate your tutor(s)",
-	"completion": "Check the box to verify the session was completed",
-	"timestamp": "Timestamp",
-	"wentWell": "What went well?",
-	"wentPoorly": "What went poorly?"
+    "name": "Student's Name:",
+    "tutor": "Tutor(s)' Name",
+    "length": "How long was your session? (in MINUTES)",
+    "rating": "How would you rate your tutor(s)",
+    "completion": "Check the box to verify the session was completed",
+    "timestamp": "Timestamp",
+    "wentWell": "What went well?",
+    "improvements": "What went poorly?",
+    "date": "When was your session?"
 }
 
